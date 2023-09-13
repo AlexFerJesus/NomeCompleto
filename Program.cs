6a7afced18ce9a80a31c917638a4d1a205a9ccb2
AlexFerJesus <alexfernandesdejesus.12@gmail.com>
2023-09-13 10:57:18 -0300

    refinamento

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿// Faça o seu nome aparecer atraves do pronpt
-string nome = Console.ReadLine()!;
-string sobrenome = Console.ReadLine()!;
-Console.WriteLine(nome + " " + sobrenome);
+﻿// Faça o seu nome aparecer atraves do Program.cs
+string nome, sobrenome;
+Console.Write("Digite seu nome.......: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome..: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
